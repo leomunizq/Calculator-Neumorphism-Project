46a9c21d9c43c5195076c9a83f823a8bdafec1f4
leomunizq <leonardo.mdq@hotmail.com>
2021-11-28 16:10:34 +0100

    New buttons and functions
    New clear all button and delete characters individually.

--- main.js
+++ main.js
@@ -12,3 +12,18 @@ var btn = form.veql
 btn.addEventListener('dblclick', function () {
   form.disp.value = '' //quando clica 2 vezes em igual limpa tudo
 })
+
+var cln = form.clean
+cln.addEventListener('click', function () {
+  form.disp.value = '' //quando clica no C limpa tudo
+})
+
+var inputText = window.document.querySelector('#disp')
+var bck = form.back
+bck.addEventListener('click', function () {
+  if (inputText.value.length) {
+    inputText.value = inputText.value.substr(0, inputText.value.length - 1)
+    inputText.focus()
+    //quando clica no < limpa
+  }
+})
